4604c32e4080aa1e8c915defbb7d6e0badfa1255
Temitope Akingbala <topesoul@gmail.com>
2024-08-03 10:39:44 +0000

    ### Revise user forms for consistency and improved validation
    Renamed `RegisterForm` to `RegistrationForm` for clarity. Added a confirm password field with validation to ensure password match. Standardized validators to use `DataRequired`. Simplified `LoginForm` by replacing email with username for uniform user identification.

--- recipehub/app/forms.py
+++ recipehub/app/forms.py
@@ -1,48 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-from app import mongo
+from wtforms.validators import DataRequired, Length, EqualTo
 
-class RegisterForm(FlaskForm):
-    username = StringField(
-        validators=[
-            InputRequired(),
-            Length(min=4, max=20)
-        ],
-        render_kw={"placeholder": "Username"}
-    )
-    email = StringField(
-        validators=[
-            InputRequired(),
-        ],
-        render_kw={"placeholder": "Email"}
-    )
-    password = PasswordField(
-        validators=[
-            InputRequired(),
-            Length(min=8, max=20)
-        ],
-        render_kw={"placeholder": "Password"}
-    )
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        user = mongo.db.users.find_one({"username": username.data})
-        if user:
-            raise ValidationError('Username already exists. Please choose a different one.')
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    email = StringField(
-        validators=[
-            InputRequired(),
-        ],
-        render_kw={"placeholder": "Email"}
-    )
-    password = PasswordField(
-        validators=[
-            InputRequired(),
-            Length(min=8, max=20)
-        ],
-        render_kw={"placeholder": "Password"}
-    )
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
