78849d44bdf430172f81c5e7c7cf49ad8489f2ba
Temitope Akingbala <topesoul@gmail.com>
2024-08-17 08:50:54 +0000

    ``` Add 'Remember Me' option to LoginForm
    Included a 'Remember Me' checkbox in the login form to enhance user experience by allowing users to stay logged in between sessions. This change required importing BooleanField from wtforms.
    ```

--- recipehub/forms.py
+++ recipehub/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -8,11 +8,10 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 class RecipeForm(FlaskForm):
