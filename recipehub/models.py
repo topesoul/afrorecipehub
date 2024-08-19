1619898e24b7df903a4945ac16c6190c2e800166
Temitope Akingbala <topesoul@gmail.com>
2024-08-19 22:28:36 +0000

    ### Add points attribute to User model
    Introduced a `points` attribute to the User model to store and manage user points more effectively. Enhanced user retrieval methods (`get_user_by_id` and `get_user_by_username`) to include the `points` field. Added a property to dynamically fetch user points from the database.

--- recipehub/models.py
+++ recipehub/models.py
@@ -3,15 +3,38 @@ from recipehub import mongo
 from bson.objectid import ObjectId
 
 class User(UserMixin):
-    def __init__(self, user_id, username, profile_image=None):
+    def __init__(self, user_id, username, profile_image=None, points=0):
         self.id = user_id
         self.username = username
         self.profile_image = profile_image
+        self._points = points
+
+    @property
+    def points(self):
+        # Retrieve the most current points from the database
+        user = mongo.db.users.find_one({"_id": ObjectId(self.id)})
+        if user:
+            return user.get("points", 0)
+        return 0
 
     @staticmethod
     def get_user_by_id(user_id):
-        return mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        if user:
+            return User(
+                str(user["_id"]),
+                user["username"],
+                user.get("profile_image"),
+                user.get("points", 0)
+            )
 
     @staticmethod
     def get_user_by_username(username):
-        return mongo.db.users.find_one({"username": username})
+        user = mongo.db.users.find_one({"username": username})
+        if user:
+            return User(
+                str(user["_id"]),
+                user["username"],
+                user.get("profile_image"),
+                user.get("points", 0)
+            )
