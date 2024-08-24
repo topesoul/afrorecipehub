d9f2ce0d424cc242267bfa14f9d8838d8d87aa0e
Temitope Akingbala <topesoul@gmail.com>
2024-08-24 12:15:42 +0000

    ``` Add docstrings and comments to clarify PyMongo initialization
    Enhanced the code with docstrings and comments to better explain
    the initialization of the Flask application with PyMongo.
    
    ```

--- recipehub/mongo.py
+++ recipehub/mongo.py
@@ -1,6 +1,13 @@
 from flask_pymongo import PyMongo
 
+# Create a PyMongo instance
 mongo = PyMongo()
 
 def init_app(app):
+    """
+    Initialize the Flask application with PyMongo.
+    
+    :param app: The Flask application instance
+    """
+    # Initialize the PyMongo instance with the Flask app
     mongo.init_app(app)
